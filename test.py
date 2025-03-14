d0cd63064f76b1a0ebec67ef34b5e0d6f928fd63
Andrey Karsakov <metall8-8@ya.ru>
2025-03-14 21:34:18 +0700

    Add new Codewars solutions

--- test.py
+++ test.py
@@ -1,5 +1,2 @@
-from functools import reduce
-
-a, q, h = input().split()
-print(f'{a=}, {q=}, {h=}')
-a = reduce()
+N = int(input())
+print((f'Я больше никогда не буду писать \"Купи слона!\"!\n') * N)
