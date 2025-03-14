8d844c6fb7ae29b9235033a8c2f038fa17eb4af1
Andrey Karsakov <metall8-8@ya.ru>
2025-03-14 10:29:22 +0700

    Add new Codewars solutions

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-from audioop import reverse
+from functools import reduce
 
-
-def square_sum(square_sum):
-    lst = square_sum.split()
-    lst2 = []
-    for word in lst:
-        if len(word) >= 5:
-            lst2.append(word[::-1])
-        else:
-            lst2.append(word)
-    str = ' '.join(lst2)
-
-    print(str)
-
-
-square_sum("Hey fellow warriors")
+a, q, h = input().split()
+print(f'{a=}, {q=}, {h=}')
+a = reduce()
